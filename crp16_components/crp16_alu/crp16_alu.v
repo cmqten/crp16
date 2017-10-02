d36930db6cf124c35af889cdf95c8d76436c6877
Carl Marquez <carl.louis.marquez@gmail.com>
2017-10-01 21:08:43 -0400

    Updated alu select bits for all operations

--- crp16_components/crp16_alu/crp16_alu.v
+++ crp16_components/crp16_alu/crp16_alu.v
@@ -4,7 +4,7 @@
 /**
  * The CRP16 ALU. Supports 8 operations. 
  */
-module crp16_alu(
+module crp16_alu (
     input [15:0] x,             // First operand
     input [15:0] y,             // Second operand
     input [2:0] select,         // Operation select
@@ -20,43 +20,7 @@ module crp16_alu(
     always @(*)
     begin
         case (select[2:0])
-            3'b000: begin // Logical shift right
-                alu_out = x >> (16'b1111 & y);
-                c = 1'b0;
-                v = 1'b0;
-            end
-            
-            3'b001: begin // Arithmetic shift right
-                alu_out = $signed(x) >>> (16'b1111 & y);
-                c = 1'b0;
-                v = 1'b0;
-            end
-            
-            3'b010: begin // Logical shift left
-                alu_out = x << (16'b1111 & y);
-                c = 1'b0;
-                v = 1'b0;
-            end
-            
-            3'b011: begin // Bitwise AND
-                alu_out = x & y;
-                c = 1'b0;
-                v = 1'b0;
-            end
-            
-            3'b100: begin // Bitwise OR
-                alu_out = x | y;
-                c = 1'b0;
-                v = 1'b0;
-            end
-            
-            3'b101: begin // Bitwise XOR
-                alu_out = x ^ y;
-                c = 1'b0;
-                v = 1'b0;
-            end
-            
-            3'b110: begin // Addition
+            3'b000: begin // Addition
                 {c, alu_out} = {1'b0, x} + {1'b0, y};// For capturing carry out
                 
                 // Addition overflows if the operands are the same sign but the
@@ -64,13 +28,49 @@ module crp16_alu(
                 v = (~(x[15] ^ y[15])) & (x[15] ^ alu_out[15]);
             end
             
-            3'b111: begin // Subtraction
+            3'b001: begin // Subtraction
                 // Two's complement addition, for capturing carry out correctly
                 {c, alu_out} = {1'b0, x} + {1'b0, ~y} + 17'b1;
                 
                 // Subtraction overflows if the operands have different signs
                 // and the result has the same sign as the second operand.
                 v = (x[15] ^ y[15]) & (~(y[15] ^ alu_out[15]));
+            end
+            
+            3'b010: begin // Logical shift right
+                alu_out = x >> (16'b1111 & y);
+                c = 1'b0;
+                v = 1'b0;
+            end
+            
+            3'b011: begin // Arithmetic shift right
+                alu_out = $signed(x) >>> (16'b1111 & y);
+                c = 1'b0;
+                v = 1'b0;
+            end
+            
+            3'b100: begin // Logical shift left
+                alu_out = x << (16'b1111 & y);
+                c = 1'b0;
+                v = 1'b0;
+            end
+            
+            3'b101: begin // Bitwise AND
+                alu_out = x & y;
+                c = 1'b0;
+                v = 1'b0;
+            end
+            
+            3'b110: begin // Bitwise OR
+                alu_out = x | y;
+                c = 1'b0;
+                v = 1'b0;
+            end
+            
+            3'b111: begin // Bitwise XOR
+                alu_out = x ^ y;
+                c = 1'b0;
+                v = 1'b0;
             end
         endcase
     end
