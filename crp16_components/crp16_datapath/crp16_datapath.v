a5e4497308d99cc73033d03ef845cbd2bf269ea7
Carl Marquez <carl.louis.marquez@gmail.com>
2018-01-06 16:24:25 -0500

    Made memory synchronous

--- crp16_components/crp16_datapath/crp16_datapath.v
+++ crp16_components/crp16_datapath/crp16_datapath.v
@@ -10,7 +10,7 @@ module crp16_datapath (
     input           clock,
     input           reset,
     
-    // Asynchronous Dual-Port RAM interface
+    // Synchronous Dual-Port RAM interface
     output  [15:0]  address_a,
     output  [15:0]  address_b,
     output  [15:0]  data_a,
@@ -21,11 +21,12 @@ module crp16_datapath (
     input   [15:0]  q_b,
     output          mem_clock, 
     
-    // Register view
+    // Datapath status view
     input   [2:0]   reg_sel,
     output  [15:0]  reg_view,
     output  [15:0]  instr_view,
-    output  [15:0]  pc_view
+    output  [15:0]  pc_view,
+    output  [15:0]  counter
 );
     /*==========================================================================
     Local Parameters, Parametrized Macros
@@ -34,17 +35,21 @@ module crp16_datapath (
     // Instruction types
     `define ALU_I(instr)        (instr[1:0] == 2'b11)   
     `define CALL_I(instr)       (instr[3:0] == 4'b1010)
+    
     `define JUMPC_I(instr)      (instr[2:0] == 3'b110)
     `define JUMPUC_I(instr)     (instr[3:0] == 4'b0010)
     `define BRANCH_I(instr)     (`CALL_I(instr) | `JUMPC_I(instr) | \
-                                 `JUMPUC_I(instr))                                
+                                 `JUMPUC_I(instr))      
+                                 
     `define GT_I(instr)         (instr[3:0] == 4'b1100)
     `define LT_I(instr)         (instr[3:0] == 4'b0100)
     `define CMP_I(instr)        (`GT_I(instr) | `LT_I(instr))
+    
     `define LOAD_I(instr)       (instr[5:0] == 6'b000001)
     `define LOADHI_I(instr)     (instr[4:0] == 5'b10001)
     `define LOADIMM_I(instr)    (instr[3:0] == 4'b1001)
     `define STORE_I(instr)      (instr[5:0] == 6'b100001)
+    
     `define STOP_I(instr)       (instr[15:0] == 16'h8000)
     
     // Encoding
@@ -79,67 +84,62 @@ module crp16_datapath (
     Data/Control Wires, Pipeline Registers
     ==========================================================================*/
     
+    // Datapath status
+    wire            stop;
+    
+  
     // Stage 0 : Instruction Fetch
-    wire    [15:0]  if_instr, if_pc;
-    wire            if_pc_wren;
+    wire    [15:0]  if_pc;
     
     wire    [15:0]  if_branch_addr, if_mem_addr, if_next_addr;
     wire            if_pc_src;
     
-    register #(16)  if_pc_r(if_next_addr, if_pc, if_pc_wren, clock, reset);
+    register #(16)  if_pc_r(if_next_addr, if_pc, ~stop, clock, reset);
     
     
     // Stage 1 : Instruction Decode 
     wire    [15:0]  dc_instr, dc_pc;
-    wire            dc_instr_wren, dc_pc_wren;
     
     wire    [2:0]   dc_rf_a_sel, dc_rf_b_sel;
     wire    [15:0]  dc_rf_a_out, dc_rf_b_out;
     wire    [15:0]  dc_reg_a_fwd, dc_reg_b_fwd, dc_imm;
     wire    [15:0]  dc_em_alu_a_in, dc_em_alu_b_in;
+    wire            dc_em_mem_wren;
     
-    register #(16)  dc_instr_r(if_instr, dc_instr, dc_instr_wren, clock, reset);
-    
-    register #(16)  dc_pc_r(if_next_addr, dc_pc, dc_pc_wren, clock, reset);
+    register #(16)  dc_pc_r(if_next_addr, dc_pc, ~stop, clock, reset);
     
     
     // Stage 2 : Execute or Memory 
     wire    [15:0]  em_instr, em_pc;
-    wire            em_instr_wren, em_pc_wren;
     wire    [15:0]  dc_em_alu_a, dc_em_alu_b, dc_em_mem_addr, dc_em_mem_data;
-    wire            dc_em_wren;
     
-    wire    [15:0]  em_alu_out, em_alu_cmp_out;
-    wire    [15:0]  em_cmp_out;
+    wire    [15:0]  em_alu_out, em_cmp_out;
     wire    [2:0]   em_alu_op;
     wire            v, c, n, z;
     
-    wire    [15:0]  em_mem_q;
-    wire            em_mem_wren;
+    wire    [15:0]  dc_em_mem_q;
     
     wire    [15:0]  em_reg_d_data;
     wire    [2:0]   em_reg_d_sel;
     wire            em_reg_wren;
     
-    register #(16)  em_instr_r(dc_instr, em_instr, em_instr_wren, clock, reset);
+    register #(16)  em_instr_r(dc_instr, em_instr, ~stop, clock, reset);
     
-    register #(16)  em_pc_r(dc_pc, em_pc, em_pc_wren, clock, reset);
+    register #(16)  em_pc_r(dc_pc, em_pc, ~stop, clock, reset);
     
-    register #(16)  dc_em_alu_a_r(dc_em_alu_a_in, dc_em_alu_a, dc_em_wren, 
+    register #(16)  dc_em_alu_a_r(dc_em_alu_a_in, dc_em_alu_a, ~stop, 
                                   clock, reset);
     
-    register #(16)  dc_em_alu_b_r(dc_em_alu_b_in, dc_em_alu_b, dc_em_wren, 
+    register #(16)  dc_em_alu_b_r(dc_em_alu_b_in, dc_em_alu_b, ~stop, 
                                   clock, reset);
+  
     
-    register #(16)  dc_em_mem_addr_r(dc_reg_a_fwd, dc_em_mem_addr, dc_em_wren, 
-                                     clock, reset);
+    // Datapath status logic
+    assign stop = `STOP_I(em_instr);
+    assign instr_view = em_instr;
+    assign pc_view    = em_pc - 16'd1;
     
-    register #(16)  dc_em_mem_data_r(dc_reg_b_fwd, dc_em_mem_data, dc_em_wren, 
-                                     clock, reset);
-    
-    
-    // Other pipeline logic
-    wire    stop = `STOP_I(em_instr);
+    register #(16)  counter_r(counter + 16'd1, counter, ~stop, clock, reset);
     
     
     /*==========================================================================
@@ -166,11 +166,11 @@ module crp16_datapath (
     // Memory
     assign mem_clock = clock;
     assign address_a = if_mem_addr;
-    assign if_instr  = q_a;
-    assign address_b = dc_em_mem_addr;
-    assign data_b    = dc_em_mem_data;
-    assign wren_b    = em_mem_wren;
-    assign em_mem_q  = q_b;
+    assign address_b = dc_reg_a_fwd;
+    assign data_b    = dc_reg_b_fwd;
+    assign wren_b    = dc_em_mem_wren;
+    assign dc_instr  = q_a;
+    assign dc_em_mem_q  = q_b;
   
     
     /*==========================================================================
@@ -179,22 +179,20 @@ module crp16_datapath (
     
     assign if_next_addr = if_mem_addr + 16'd1;
     assign if_mem_addr  = if_pc_src ? if_branch_addr : if_pc;
-    assign if_pc_wren   = ~stop;
     
     
     /*==========================================================================
     Decode Stage Logic
     ==========================================================================*/
     
-    assign dc_instr_wren  = ~stop;
-    assign dc_pc_wren     = ~stop;
-    
+    // Immediate source
     assign dc_imm = `LOADIMM_I(dc_instr)   ?
                         (`SIGNED(dc_instr) ? `SE8(dc_instr) : `ZE8(dc_instr)) :
                     `ALU_I(dc_instr) | `CMP_I(dc_instr) ?
                         (`SIGNED(dc_instr) ? `SE4(dc_instr) : `ZE4(dc_instr)) :
                     16'h0;
     
+    // Register values and forwarding
     assign dc_rf_a_sel    = `REGA(dc_instr);
     
     assign dc_rf_b_sel    = `JUMPC_I(dc_instr) | `LOADHI_I(dc_instr) |
@@ -207,7 +205,7 @@ module crp16_datapath (
     assign dc_reg_b_fwd   = em_reg_wren & (em_reg_d_sel == dc_rf_b_sel) ?
                             em_reg_d_data : dc_rf_b_out;
               
-    // Operands          
+    // ALU Operands          
     assign dc_em_alu_a_in = `LOADHI_I(dc_instr)  ? `HI8(dc_instr) :
                             `LOADIMM_I(dc_instr) ? 16'b0 : 
                             dc_reg_a_fwd;
@@ -215,6 +213,9 @@ module crp16_datapath (
     assign dc_em_alu_b_in = `LOADHI_I(dc_instr)  ? dc_reg_b_fwd[7:0] :
                             `LOADIMM_I(dc_instr) | `IMM(dc_instr) ? dc_imm : 
                             dc_reg_b_fwd;
+    
+    // Memory
+    assign dc_em_mem_wren = ~stop & `STORE_I(dc_instr);
     
     // Branch address is 0xdead if not a branch instruction for easy error 
     // detection
@@ -232,11 +233,6 @@ module crp16_datapath (
     Execute or Memory Stage Logic
     ==========================================================================*/
     
-    // Pipeline registers
-    assign em_instr_wren   = ~stop;
-    assign em_pc_wren      = ~stop;
-    assign dc_em_wren      = ~stop;
-    
     // - Subtraction for lt/gt to get result from ALU flags
     // - Addition for loadi/loadhi for obvious reasons
     // - Doesn't matter for everything else
@@ -251,32 +247,21 @@ module crp16_datapath (
                              (`SIGNED(em_instr) ? (n ^ v) : ~c) :
                           1'b0)};
     
-    assign em_alu_cmp_out = `ALU_I(em_instr)     ? em_alu_out :
-                            `CMP_I(em_instr)     ? em_cmp_out :
-                            `LOADIMM_I(em_instr) ? em_alu_out :
-                            `LOADHI_I(em_instr)  ? em_alu_out :
-                            16'hdead;
-    
-    assign em_mem_wren   = ~stop & `STORE_I(em_instr);
-    
     assign em_reg_d_sel  = `CALL_I(em_instr) ? LINK_REG : `REGD(em_instr);
     
     // Write 0xdead to register if instruction doesn't match any of the 
     // following instructions for easy error detection
     assign em_reg_d_data = `CALL_I(em_instr) ? em_pc : 
-                           `LOAD_I(em_instr) ? em_mem_q :
-                           (`ALU_I(em_instr)    | `LOADIMM_I(em_instr) |
-                            `LOADHI_I(em_instr) | `CMP_I(em_instr)) ? 
-                                em_alu_cmp_out :
+                           `LOAD_I(em_instr) ? dc_em_mem_q :
+                           `CMP_I(em_instr)  ? em_cmp_out :
+                           (`ALU_I(em_instr) | `LOADIMM_I(em_instr) |
+                            `LOADHI_I(em_instr)) ? em_alu_out :
                            16'hdead;
     
     assign em_reg_wren   = ~stop & 
                            (`ALU_I(em_instr)  | `LOADIMM_I(em_instr) | 
                             `LOAD_I(em_instr) | `LOADHI_I(em_instr)  | 
                             `CALL_I(em_instr) | `CMP_I(em_instr));  
-    
-    assign instr_view = em_instr;
-    assign pc_view    = em_pc - 16'd1;
     
 endmodule
 
--- crp16_components/crp16_processor/crp16_processor.v
+++ crp16_components/crp16_processor/crp16_processor.v
@@ -4,9 +4,13 @@
 `include "../crp16_datapath/crp16_datapath.v"
 `include "../../crp16_subcomponents/hex_decoder/hex_decoder.v"
 
+// Clock period / 2
+`define HI_PERIOD 32'd12499999  
+
 module crp16_processor (
     input   [3:0]   KEY,
     input   [9:0]   SW,
+    input           CLOCK_50,
     output  [9:0]   LEDR,
     output  [6:0]   HEX0,
     output  [6:0]   HEX1,
@@ -17,30 +21,39 @@ module crp16_processor (
 );
 
     wire    [15:0]  address_a, address_b, data_a, data_b, q_a, q_b;
-    wire            wren_a, wren_b, mem_clock, reset;
-    wire    [15:0]  hex_in, reg_view, instr_view, pc_view;
+    wire            wren_a, wren_b, mem_clock, clock, reset;
+    wire    [15:0]  hex_in, counter, reg_view, instr_view, pc_view;
     
-    assign reset = ~KEY[1];
+    // Clock divider
+    reg             clock_div = 1'b0;
+    reg     [31:0]  clock_counter = `HI_PERIOD;
     
+    assign reset     = ~KEY[1];
+    assign clock     = clock_div;
+    
+    // Dual port memory
     dual_mem memory (
-        .clock(~mem_clock), 
+        .clock(clock), 
         .address_a(address_a),  .address_b(address_b), 
         .data_a(data_a),        .data_b(data_b),
         .wren_a(wren_a),        .wren_b(wren_b),
         .q_a(q_a),              .q_b(q_b)
     );
     
+    // Datapath
     crp16_datapath datapath (
-        .clock(KEY[0]),             .reset(reset),
+        .clock(clock),              .reset(reset),
         .address_a(address_a),      .address_b(address_b), 
         .data_a(data_a),            .data_b(data_b),
         .wren_a(wren_a),            .wren_b(wren_b),
         .q_a(q_a),                  .q_b(q_b),       
         .mem_clock(mem_clock),
         .reg_sel(SW[2:0]),          .reg_view(reg_view), 
-        .instr_view(instr_view),    .pc_view(pc_view)
+        .instr_view(instr_view),    .pc_view(pc_view),
+        .counter(counter)
     );
     
+    // 7 segment viewer
     hex_decoder addrlo(pc_view[3:0], HEX4);
     hex_decoder addrhi(pc_view[4], HEX5);
     hex_decoder h0(hex_in[3:0], HEX0);
@@ -48,7 +61,21 @@ module crp16_processor (
     hex_decoder h2(hex_in[11:8], HEX2);
     hex_decoder h3(hex_in[15:12], HEX3);
     
-    assign hex_in = SW[3] ? reg_view : instr_view;
+    assign hex_in = SW[4] ? counter  :
+                    SW[3] ? reg_view : 
+                    instr_view;
+    
+    // 2 Hz clock
+    always @(posedge CLOCK_50)
+    begin
+        if (clock_counter > 32'd0) clock_counter <= clock_counter - 1;
+        
+        else
+        begin
+            clock_counter <= `HI_PERIOD;
+            clock_div <= clock_div ^ 1'b1;
+        end
+    end
 
 endmodule
 
