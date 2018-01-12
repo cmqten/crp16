81a257df3f5b6456ff2acac14221d2c1202dd20c
Carl Marquez <carl.louis.marquez@gmail.com>
2018-01-12 11:22:17 -0500

    Parsing ldi, ldsi, ldhi

--- assembler/cas.py
+++ assembler/cas.py
@@ -1,6 +1,7 @@
 from typing import List
 import re
 
+ARGS2_REGEX = re.compile("^\s*(\S+)\s*,\s*(\S+)\s*")
 ARGS3_REGEX = re.compile("^\s*(\S+)\s*,\s*(\S+)\s*,\s*(\S+)\s*")
 COMMENT_REGEX = re.compile("^\s*(.*);(.*)$")
 INSTR_REGEX = re.compile("^\s*(\S+)\s+(.*)$")
@@ -45,9 +46,9 @@ INSTR_PARSER = {
     "jez"  : None,
     "jmp"  : None,
     "jnz"  : None,
-    "ldhi" : None,
-    "ldi"  : None,
-    "ldsi" : None,
+    "ldhi" : lambda x, y: parse_load_imm_statement(x, y),
+    "ldi"  : lambda x, y: parse_load_imm_statement(x, y),
+    "ldsi" : lambda x, y: parse_load_imm_statement(x, y),
     "ldw"  : None,  
     "lt"   : lambda x, y: parse_alu_statement(x, y),
     "lts"  : lambda x, y: parse_alu_statement(x, y),
@@ -72,6 +73,23 @@ def print_int_bin(num: int, n: int) -> None:
     print(num & 1)
 
 
+def parse_int_literal(literal_str: str) -> int:
+    '''
+    Converts the string representation of an integer literal into an integer.
+    '''
+    try:
+        if literal_str.startswith("0b"): return int(literal_str, base=2)
+
+        elif literal_str.startswith("0o"): return int(literal_str, base=8)
+
+        elif literal_str.startswith("0x"): return int(literal_str, base=16)
+
+        else: return int(literal_str)
+
+    except:
+        return None
+
+    
 def parse_alu_statement(instr: str, args: str) -> int:
     '''
     Parses instructions that use the ALU: add, and, gt, gts, lt, lts, or, sll,
@@ -87,8 +105,8 @@ def parse_alu_statement(instr: str, args: str) -> int:
     dest = args_match_result.group(1)
     op_a = args_match_result.group(2)
     op_b = args_match_result.group(3)
-    imm = 0
-    machine_code = INSTRUCTIONS[instr]
+    imm = 0  
+    opcode = INSTRUCTIONS[instr]
 
     if dest not in REGISTERS:
         print("Error: first argument must be a valid register", end="")
@@ -106,29 +124,51 @@ def parse_alu_statement(instr: str, args: str) -> int:
     # of the four bits reserved for the third argument
     if op_b in REGISTERS: op_b = REGISTERS[op_b] << 1
 
-    # Literal third argument
+    # Integer literal third argument
     else:
-        try:
-            imm = 1
-        
-            if op_b.isnumeric() or op_b.startswith("-"): op_b = int(op_b) & 15
+        imm = 1
+        op_b = parse_int_literal(op_b)
 
-            elif op_b.startswith("0b"): op_b = int(op_b, base=2) & 15
-
-            elif op_b.startswith("0o"): op_b = int(op_b, base=8) & 15
-
-            elif op_b.startswith("0x"): op_b = int(op_b, base=16) & 15
-
-            else:
-                print("Error: third argument must be a register or", end="")
-                print(" an integer literal", end="")
-                return None
-
-        except:
-            print("Error: invalid literal", end="")
+        if op_b is None:
+            print("Error: third argument must be a register", end="")
+            print(" or an integer literal", end="")
             return None
 
-    return machine_code | imm << 5 | dest << 13 | op_a << 10 | op_b << 6
+        op_b &= 15 # Only 4 bits of immediate
+
+    return opcode | imm << 5 | dest << 13 | op_a << 10 | op_b << 6
+
+
+def parse_load_imm_statement(instr: str, args: str) -> int:
+    '''
+    Parses ldi, ldsi, ldhi instructions.
+    '''
+    args_match_result = ARGS2_REGEX.match(args)
+
+    if not args_match_result:
+        print("Error: invalid arguments", end="")
+        return None
+
+    opcode = INSTRUCTIONS[instr]
+    dest = args_match_result.group(1)
+    immop = args_match_result.group(2)
+    
+    if dest not in REGISTERS:
+        print("Error: first argument must be a valid register", end="")
+        return None
+
+    dest = REGISTERS[dest]  # Integer equivalent of the register number
+
+    # Second argument is an integer literal
+    immop = parse_int_literal(immop)
+
+    if immop is None:
+        print("Error: second argument must be an integer literal", end="")
+        return None
+
+    immop &= 255 # Only 8 bits of immediate
+
+    return opcode | immop << 5 | dest << 13
 
 
 def get_label(line: str) -> List[str]:
